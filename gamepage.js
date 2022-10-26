b1d5319f03f94e92fbd6c0e283faebcfc19cc6fe
riyanshiyo <109360682+riyanshiyo@users.noreply.github.com>
2022-10-26 21:06:34 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,33 @@
+// random value generated
+var y = Math.floor(Math.random() * 10 + 1); 
+// counting the number of guesses
+// made for correct Guess
+
+  
+// function for number guessed by user 
+var x = document.getElementById("guessField").value; 
+
+function submit()
+{
+    if (x == y)
+    { 
+    alert("CONGRATULATIONS!!! "+playername+" YOU GUESSED IT RIGHT IN + guess + GUESS ");
+     guess= 1;
+    }
+
+    else if(x > y) /* if guessed number is greater than actual number*/
+{
+
+guess++;
+alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+guess++;
+alert("OOPS SORRY!! TRY A GREATER NUMBER")
+}
+
+}
+
+function playAgain(){ y = Math.floor(Math.random() * 10 + 1);
+}
